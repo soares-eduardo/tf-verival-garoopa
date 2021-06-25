93dd8df2a1055eddb556d1f2f74c2af760db4c5f
Eduardo Soares <eduardoandre.mcc@gmail.com>
2021-06-25 16:28:41 +0000

    Finalizei teste da serviço

--- src/test/java/com/eduardojoao/ServicoPassageiroTest.java
+++ src/test/java/com/eduardojoao/ServicoPassageiroTest.java
@@ -4,7 +4,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
-import java.time.LocalDateTime;
 import java.util.List;
 import java.util.stream.Collectors;
 
