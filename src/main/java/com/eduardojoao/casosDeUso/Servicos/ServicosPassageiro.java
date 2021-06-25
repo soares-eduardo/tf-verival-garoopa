521415abdd4d2df6701ba480d705d9abeb55f1a0
Eduardo Soares <eduardoandre.mcc@gmail.com>
2021-06-25 16:13:15 +0000

    Classe servico testada

--- src/main/java/com/eduardojoao/casosDeUso/Servicos/ServicosPassageiro.java
+++ src/main/java/com/eduardojoao/casosDeUso/Servicos/ServicosPassageiro.java
@@ -26,31 +26,25 @@ public class ServicosPassageiro {
         this.custoViagem = new CustoViagem(ccv);
     }
 
-    public List<String> getListaBairros(){
-        return repBairros.recuperaListaBairros()
-                .stream()
-                .map(b->b.getNome())
-                .collect(Collectors.toList());
+    public List<String> getListaBairros() {
+        return repBairros.recuperaListaBairros().stream().map(b -> b.getNome()).collect(Collectors.toList());
     }
 
-    public List<String> getPassageirosCadastrados(){
-        return repPassageiros.listaPassageiros()
-                .stream()
-                .map(p->p.getNome())
-                .collect(Collectors.toList());
+    public List<String> getPassageirosCadastrados() {
+        return repPassageiros.listaPassageiros().stream().map(p -> p.getNome()).collect(Collectors.toList());
     }
 
-    public Roteiro criaRoteiro(String bairroOrigem,String bairroDestino){
+    public Roteiro criaRoteiro(String bairroOrigem, String bairroDestino) {
         Collection<Bairro> todosBairros = repBairros.recuperaListaBairros();
         Bairro bOrigem = repBairros.recuperaPorNome(bairroOrigem);
         Bairro bDestino = repBairros.recuperaPorNome(bairroDestino);
-        return new Roteiro(bOrigem,bDestino,todosBairros);
+        return new Roteiro(bOrigem, bDestino, todosBairros);
     }
 
-    public Viagem criaViagem(int id,Roteiro roteiro,String cpfPassageiro){
+    public Viagem criaViagem(int id, Roteiro roteiro, String cpfPassageiro) {
         LocalDateTime data = LocalDateTime.now();
         Passageiro passageiro = repPassageiros.recuperaPorCPF(cpfPassageiro);
         double valorCobrado = custoViagem.custoViagem(roteiro, passageiro);
-        return new Viagem(id,data,roteiro,passageiro,valorCobrado);
+        return new Viagem(id, data, roteiro, passageiro, valorCobrado);
     }
 }
--- src/test/java/com/eduardojoao/ServicoPassageiroTest.java
+++ src/test/java/com/eduardojoao/ServicoPassageiroTest.java
@@ -4,8 +4,8 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
+import java.time.LocalDateTime;
 import java.util.List;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 import com.eduardojoao.casosDeUso.Politicas.CalculoCustoViagem;
@@ -16,6 +16,7 @@ import com.eduardojoao.casosDeUso.Servicos.ServicosPassageiro;
 import com.eduardojoao.entidades.Bairro;
 import com.eduardojoao.entidades.Passageiro;
 import com.eduardojoao.entidades.Roteiro;
+import com.eduardojoao.entidades.Viagem;
 import com.eduardojoao.entidades.geometria.Area;
 import com.eduardojoao.entidades.geometria.Ponto;
 
@@ -26,9 +27,9 @@ public class ServicoPassageiroTest {
 
     private RepositorioBairros repositorioBairros;
     private RepositorioPassageiros repositorioPassageiros;
-    private CustoViagem custoViagem;
     private ServicosPassageiro servicosPassageiro;
     private Bairro bairro1, bairro2;
+    private Passageiro passageiro;
 
     @BeforeEach
     void setUp() {
@@ -46,7 +47,7 @@ public class ServicoPassageiroTest {
         when(repositorioBairros.recuperaPorNome("Bom Fim")).thenReturn(bairro1);
         when(repositorioBairros.recuperaPorNome("Menino Deus")).thenReturn(bairro2);
 
-        Passageiro passageiro = Passageiro.novoPassageiro("60076180050", "Eduardo");
+        passageiro = Passageiro.novoPassageiro("60076180050", "Eduardo");
 
         repositorioPassageiros = mock(RepositorioPassageiros.class);
         when(repositorioPassageiros.listaPassageiros()).thenReturn(List.of(
@@ -57,7 +58,7 @@ public class ServicoPassageiroTest {
         when(repositorioPassageiros.recuperaPorCPF("60076180050")).thenReturn(passageiro);
 
         CalculoCustoViagem ccv = mock(CalculoCustoViagem.class);
-
+        
         servicosPassageiro = new ServicosPassageiro(repositorioBairros, repositorioPassageiros, ccv);
     }
 
@@ -85,5 +86,20 @@ public class ServicoPassageiroTest {
     public void testarCriaRoteiro() {
         Roteiro roteiroAtual = servicosPassageiro.criaRoteiro("Bom Fim", "Menino Deus");
         assertEquals(new Roteiro(bairro1, bairro2, repositorioBairros.recuperaListaBairros()), roteiroAtual);
+    }
+
+    @Test
+    public void testarCriaViagem() {
+        Roteiro roteiro = mock(Roteiro.class);
+        CustoViagem cv = mock(CustoViagem.class);
+
+        Viagem viagemAtual = servicosPassageiro.criaViagem(1, roteiro, "60076180050");
+        Viagem viagemEsperada = new Viagem(1, viagemAtual.getDataHora(), roteiro, passageiro, cv.custoViagem(roteiro, passageiro));
+        
+        assertEquals(viagemEsperada.getId(), viagemAtual.getId());
+        assertEquals(viagemEsperada.getDataHora(), viagemAtual.getDataHora());
+        assertEquals(viagemEsperada.getRoteiro(), viagemAtual.getRoteiro());
+        assertEquals(viagemEsperada.getPassageiro(), viagemAtual.getPassageiro());
+        assertEquals(viagemEsperada.getValorCobrado(), viagemAtual.getValorCobrado());
     }
 }
